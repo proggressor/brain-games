6f0bbd7960c16e5d1047dbe22ba9e36a7647f780
Vorobyev <nikos1997@yandex.ru>
2022-12-04 23:20:14 +0300

    fix prime_logic

--- brain_games/games/prime_logic.py
+++ brain_games/games/prime_logic.py
@@ -6,12 +6,12 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_game():
-    example = randint(0, 100)
+    example = randint(2, 100)
     right_answer = 'yes'
     divider = 2
 
     while divider <= sqrt(example):
-        if example % divider == 0 or example in [0, 1]:
+        if example % divider == 0:
             right_answer = 'no'
             return example, right_answer
         divider += 1
